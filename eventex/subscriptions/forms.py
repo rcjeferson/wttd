c697f242931f142ef44a4775b0ef7816917dffec
Jeferson Costa <jeferson.costa@instruct.com.br>
2020-01-28 18:58:38 -0300

    feat(validação): Validação campos
    Validação dos campos de CPF, Email e Telefone.

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -1,7 +1,30 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
+
+def validate_cpf(value):
+    if not value.isdigit():
+        raise ValidationError('CPF deve conter apenas números', 'cpf_digit')
+
+    if len(value) != 11:
+        raise ValidationError('CPF deve conter 11 números', 'cpf_length')
+
 
 class SubscriptionForm(forms.Form):
     name = forms.CharField(label='Nome')
-    cpf = forms.CharField(label='CPF')
-    email = forms.EmailField(label='Email')
-    phone = forms.CharField(label='Telefone')
+    cpf = forms.CharField(label='CPF', validators=[validate_cpf])
+    email = forms.EmailField(label='Email', required=False)
+    phone = forms.CharField(label='Telefone', required=False)
+
+    def clean_name(self):
+        name = self.cleaned_data['name']
+
+        words = []
+        for w in name.split():
+            words.append(w.capitalize())
+
+        return ' '.join(words)
+
+    def clean(self):
+        if not self.cleaned_data.get('email') and not self.cleaned_data.get('phone'):
+            raise ValidationError('Informe seu Email ou telefone.')
--- eventex/subscriptions/tests/test_form_subscription.py
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -1,3 +1,4 @@
+from django.shortcuts import resolve_url as r
 from django.test import TestCase
 
 from eventex.subscriptions.forms import SubscriptionForm
@@ -11,3 +12,67 @@ class SubscriptionFormTest(TestCase):
         """Form must have 4 fields"""
         expected = ['name', 'cpf', 'email', 'phone']
         self.assertSequenceEqual(expected, list(self.form.fields))
+
+    def test_cpf_is_digit(self):
+        """CPF must only accept digits"""
+        form = self.make_validated_form(cpf='ABCD1234567')
+        self.assertFormErrorCode(form, 'cpf', 'cpf_digit')
+
+    def test_cpf_has_11_digits(self):
+        """CPF must have 11 digits"""
+        form = self.make_validated_form(cpf='1234')
+        self.assertFormErrorCode(form, 'cpf', 'cpf_length')
+
+    def test_name_must_be_capitalized(self):
+        """Name Must be capitalized"""
+        form = self.make_validated_form(name='JEFERSON Costa')
+        self.assertEqual(form.cleaned_data['name'], 'Jeferson Costa')
+
+    def test_email_is_optional(self):
+        """Email is optional"""
+        form = self.make_validated_form(email='')
+        self.assertFalse(form.errors)
+
+    def test_phone_is_optiona(self):
+        """Phone is optional"""
+        form = self.make_validated_form(phone='')
+        self.assertFalse(form.errors)
+
+    def test_must_inform_email_or_phone(self):
+        """Email and Phone are optional, but one must be informed"""
+        form = self.make_validated_form(email='', phone='')
+        self.assertListEqual(['__all__'], list(form.errors))
+
+    def test_must_email_or_phone(self):
+        """Email or Phone are optional"""
+        form = self.make_validated_form(email='teste@eventex.com', phone='')
+        self.assertFalse(form.errors)
+
+    def assertFormErrorCode(self, form, field, code):
+        errors = form.errors.as_data()
+        errors_list = errors[field]
+        exception = errors_list[0]
+        self.assertEqual(code, exception.code)
+
+    def make_validated_form(self, **kwargs):
+        valid = dict(
+            name='Jeferson Costa',
+            cpf='12345678901',
+            email='email@teste.com',
+            phone='99999999999'
+        )
+        data = dict(valid, **kwargs)
+        form = SubscriptionForm(data)
+        form.is_valid()
+        return form
+
+
+class TemplateRegressionTest(TestCase):
+    def test_template_has_non_field_errors(self):
+        invalid_data = dict(
+            nome = 'Jeferson Costa',
+            cpf = '12345678901',
+        )
+
+        response = self.client.post(r('subscriptions:new'), invalid_data)
+        self.assertContains(response, '<ul class="errorlist nonfield">')
