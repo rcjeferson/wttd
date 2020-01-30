597ac79590d6670e82f01fdf648dfbb6986664f2
Jeferson Costa <jeferson.costa@instruct.com.br>
2020-01-30 08:37:25 -0300

    refactor(subscription): Refatora Form
    Refatora form para utilizar ModelForm.

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -1,30 +1,21 @@
 from django import forms
 from django.core.exceptions import ValidationError
 
-
-def validate_cpf(value):
-    if not value.isdigit():
-        raise ValidationError('CPF deve conter apenas números', 'cpf_digit')
-
-    if len(value) != 11:
-        raise ValidationError('CPF deve conter 11 números', 'cpf_length')
+from eventex.subscriptions.models import Subscription
 
 
-class SubscriptionForm(forms.Form):
-    name = forms.CharField(label='Nome')
-    cpf = forms.CharField(label='CPF', validators=[validate_cpf])
-    email = forms.EmailField(label='Email', required=False)
-    phone = forms.CharField(label='Telefone', required=False)
+class SubscriptionForm(forms.ModelForm):
+    class Meta:
+        model = Subscription
+        fields = ['name', 'cpf', 'email', 'phone']
 
     def clean_name(self):
         name = self.cleaned_data['name']
-
-        words = []
-        for w in name.split():
-            words.append(w.capitalize())
-
+        words = [w.capitalize() for w in name.split()]
         return ' '.join(words)
 
     def clean(self):
+        self.cleaned_data = super().clean()
+
         if not self.cleaned_data.get('email') and not self.cleaned_data.get('phone'):
             raise ValidationError('Informe seu Email ou telefone.')
